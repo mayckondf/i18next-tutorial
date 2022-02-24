de40cafb904a9434d0136fb8ab862a2de8ecdec1
Mayckon DF <mayckon.fr@gmail.com>
2022-02-23 23:45:52 -0300

    feat: i18next basic

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+import './src/assets/translation/i18next'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
+  const {t} = useTranslation()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{t('greeting')}</Text>
       <StatusBar style="auto" />
     </View>
   );
